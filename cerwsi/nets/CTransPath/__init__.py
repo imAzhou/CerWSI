19d1c920bfa7e9ce446ce7b1ba58c5574b049012
imAzhou <xiaoxiangyafei_1@163.com>
2025-03-11 16:06:43 +0800

    0311-0

---
+++ cerwsi/nets/CTransPath/__init__.py
@@ -0,0 +1,1 @@
+from .ctran import ctranspath
--- cerwsi/nets/__init__.py
+++ cerwsi/nets/__init__.py
@@ -1,10 +1,4 @@
 from .valid_cls_net import ValidClsNet
-from .patch_cls_net import PatchClsNet
-# from .patch_multicls_net import PatchMultiClsNet
-# from .patch_multihead_net import PatchMultiHeadNet
-from .slide_cls_net import SlideClsNet
-# from .patch_cls_dino import PatchClsDINO
-from .multi_patch_uni_v2 import MultiPatchUNI
 from .CerMCNet import CerMCNet
 
 from .multilabel_baseline.resnet import MultiResNet

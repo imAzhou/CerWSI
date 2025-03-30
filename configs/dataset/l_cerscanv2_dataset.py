7d3d75e9ba1e30f338f27a6a02dd17728ce7de19
imAzhou <xiaoxiangyafei_1@163.com>
2025-03-30 06:59:47 +0000

    0330

--- configs/dataset/l_cerscanv2_dataset.py
+++ configs/dataset/l_cerscanv2_dataset.py
@@ -1,5 +1,5 @@
 # dataset settings 
-data_root = '/x22201018/datasets/CervicalDatasets/data_resource'
+data_root = '/x22201018/datasets/CervicalDatasets/LCerScanv2'
 # data_root = '/disk/medical_datasets/cervix/data_resource'
 # data_root = '/c22073/zly/datasets/CervicalDatasets/LCerScanv2'
 classes = ['negative', 'ASC-US', 'LSIL', 'ASC-H', 'HSIL', 'AGC']
@@ -7,7 +7,7 @@ classes = ['negative', 'ASC-US', 'LSIL', 'ASC-H', 'HSIL', 'AGC']
 num_classes = len(classes)
 train_bs = 128
 val_bs = 128
-split_group = 1     # 一般情况下设置为1，不等于1时，会分 split_group 组数据分别依次送入backbone抽特征后，组合到一起送入解码器中训练
+split_group = 8     # 一般情况下设置为1，不等于1时，会分 split_group 组数据分别依次送入backbone抽特征后，组合到一起送入解码器中训练
 
 train_annojson = f'{data_root}/annofiles/train_patches_v0319.json'
 val_annojson = f'{data_root}/annofiles/val_patches_v0319.json'

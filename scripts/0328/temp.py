93ceacb84870d289b5564c6e18b46f33d1475364
AZhou <xiaoxiangyafei_1@163.com>
2025-04-10 19:26:50 +0800

    137

--- scripts/0328/temp.py
+++ scripts/0328/temp.py
@@ -1,38 +1,21 @@
 import json
 from tqdm import tqdm
 import os
+import glob
 import shutil
 
-classes = ['NILM', 'AGC', 'ASC-US', 'LSIL', 'ASC-H', 'HSIL']
-
-pn_cnt = [0,0]
-# for mode in ['train', 'val']:
 for mode in ['val']:
-    with open(f'data_resource/0328/annofiles/{mode}_patches_v0328.json', 'r') as f:
+    cnt = 0
+    with open(f'data_resource/0410/annofiles/{mode}_v0410.json', 'r') as f:
         patch_list = json.load(f)
-    mode_pn_cnt = [0,0]
-    all_neg_imgs = os.listdir('data_resource/0328/images/Neg')
     for patchinfo in tqdm(patch_list, ncols=80):
-        pn_cnt[patchinfo['diagnose']] += 1
-        mode_pn_cnt[patchinfo['diagnose']] += 1
-        # if random.random() > 0.01:
-        #     continue
-        if patchinfo['diagnose'] == 0 and patchinfo['filename'] not in all_neg_imgs:
-            shutil.move(
-                f'data_resource/0328/images/Pos/{patchinfo["filename"]}',
-                f'data_resource/0328/images/Neg/{patchinfo["filename"]}'
-            )
-        # new_clsname,new_bbox = [],[]
-        # for sub_class,bbox in zip(patchinfo['clsnames'], patchinfo['bboxes']):
-
-        #         new_clsname.append(sub_class)
-        #         new_bbox.append(bbox)
-        # patchinfo['clsnames'] = new_clsname
-        # patchinfo['bboxes'] = new_bbox
-        # if len(new_bbox) == 0:
-        #     patchinfo['diagnose'] = 0
-
-#     with open(f'data_resource/0328/annofiles/{mode}_patches_v0328.json', 'w') as f:
-#         json.dump(patch_list,f)
-#     print(f'mode {mode}: {mode_pn_cnt}')
-# print(pn_cnt)
+        if '0403jfsw' in patchinfo['prefix'] and patchinfo['diagnose'] == 1:
+            purename = patchinfo['filename'].split('.')[0]
+            src_path = f'data_resource/0403/images/Pos/{purename}.png'
+            target_path = f'Pos_temp/{purename}.png'
+            try:
+                shutil.copy(src_path, target_path)
+                cnt += 1
+            except:
+                print(f'File not exist: {src_path}')
+    print(f'mode: {mode}: {cnt}')    
